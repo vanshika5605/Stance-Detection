c53f8b37e2e728d007d55547951f0c60ea3ebe3e
Vanshika Agrawal <vanshikaagrawal56@gmail.com>
2021-04-06 22:11:24 +0530

    Stance and Sarcasm done

--- analysis/models.py
+++ analysis/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     content = models.CharField(max_length=250,blank=True, null=True)
     img = models.ImageField(upload_to='postpics',blank=True, null=True)
     target = models.CharField(max_length=100,blank=True, null=True)
-    sarcasm = models.IntegerField(blank=True, null=True)
+    sarcasm = models.CharField(max_length=20,blank=True, null=True)
     sentiment = models.CharField(max_length=20,blank=True, null=True)
     stance = models.CharField(max_length=20,blank=True, null=True)
     date_added = models.CharField(max_length=100, blank=True, null=True)
--- analysis/views.py
+++ analysis/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render, redirect
 from .models import Post
+import pandas as pd
 # Create your views here.
 
 def display(request):
     posts = Post.objects.all()
+    sarcasm_model=pd.read_pickle(r'C:\Users\HP\MinorProject\minor\models\SarcasmDetection\model.pkl')
+    stance_model=pd.read_pickle(r'C:\Users\HP\MinorProject\minor\models\StanceDetection\stance.pkl')
+    vectorizer=pd.read_pickle(r'C:\Users\HP\MinorProject\minor\models\SarcasmDetection\tfidf.pkl')
+    for post in posts:
+        s=[]
+        s.append(post.content)
+        data=vectorizer.transform(s).toarray()
+        result1=sarcasm_model.predict(data)
+        if result1:
+            post.sarcasm="Yes"
+        else:
+            post.sarcasm="No"
+        result2=stance_model.predict(s)
+        post.stance=result2[0]
+        post.save()
 
     return render(request, "display.html", {'posts': posts})
 
@@ -17,6 +33,6 @@ def post(request):
 
         post=Post(content=content, target=target, img=img, date_added=date, author=author)
         post.save()
-        return redirect('/')
+        return redirect('/analysis')
     else:
         return render(request, "addpost.html")
